757593aa87c60014393c1f0fb24f31538a992a94
Oleg Safonov <by.xsofa@gmail.com>
2018-04-24 16:52:30 +0300

    Fix args parsing, remove empty id properties

--- catalog/meta.go
+++ catalog/meta.go
@@ -3,7 +3,7 @@ package catalog
 // Meta structure describes engine metadata used in linterhub
 type Meta struct {
 	// Engine id, case insensitive unique identifier, required only if the engine name is not unique
-	ID string `json:"id"`
+	ID string `json:"id,omitempty"`
 
 	// Case sensitive official engine name, may not be unique
 	Name string `json:"name"`
